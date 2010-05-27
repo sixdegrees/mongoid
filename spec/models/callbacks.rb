86e607e38d37c757dd33582445680ff6090be7a5
Cyril Mougel <cyril@yeastymobs.com>
2010-05-27 12:30:09 +0200

    we need made callback on validation. Not validate add integration spec about validate callback

--- spec/models/callbacks.rb
+++ spec/models/callbacks.rb
@@ -21,3 +21,14 @@ class Song
   field :title
   embedded_in :artist, :inverse_of => :songs
 end
+
+class ValidationCallback
+  include Mongoid::Document
+  field :history, :type => Array, :default => []
+  validate do
+    self.history << :validate
+  end
+
+  before_validation { self.history << :before_validation }
+  after_validation { self.history << :after_validation }
+end
